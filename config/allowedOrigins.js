e8624359a758ba924b57a800a2f8083ed5632c9e
n-muhammad-taufiq <n.muhammad.taufiq@outlook.com>
2025-10-01 16:17:14 +0530

    added origin in allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,4 @@
 //replace origin in production
-const allowedOrigins=['https://tectraclinic.vercel.app'];
+const allowedOrigins=['https://tectraclinic.vercel.app','http://localhost:5173'];
 
 module.exports=allowedOrigins;  
